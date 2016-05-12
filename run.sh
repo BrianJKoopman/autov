e401cc0b0e717f39a78668145ead97384c22f95f
Brian Koopman <bjk98@cornell.edu>
2016-05-12 11:09:56 -0400

    added a pa2 run

--- run.sh
+++ run.sh
@@ -1,2 +1,3 @@
 python wavelength_study.py ./cfg/pa3_wl_study_50_250_ghz_rahuls_coating.json
 python wavelength_study.py ./cfg/pa3_wl_study_10_420_ghz_rahuls_coating.json
+python wavelength_study.py ./cfg/pa2_wl_study_50_250_ghz_joes_coatings.json
