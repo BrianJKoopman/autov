6208f5145d6b36bcbf37bbc69a3671464893848a
Brian Koopman <bjk98@cornell.edu>
2016-09-29 18:27:01 -0400

    adjust run for new data run

--- run.sh
+++ run.sh
@@ -1,3 +1,7 @@
-python wavelength_study.py ./cfg/pa3_wl_study_50_250_ghz_rahuls_coating.json
-python wavelength_study.py ./cfg/pa3_wl_study_10_420_ghz_rahuls_coating.json
-python wavelength_study.py ./cfg/pa2_wl_study_50_250_ghz_joes_coatings.json
+#python wavelength_study.py ./cfg/pa3_wl_study_50_250_ghz_rahuls_coating.json
+#python wavelength_study.py ./cfg/pa3_wl_study_10_420_ghz_rahuls_coating.json
+#python wavelength_study.py ./cfg/pa2_wl_study_50_250_ghz_joes_coatings.json
+
+python single_wavelength_run.py 1
+python single_wavelength_run.py 2
+python single_wavelength_run.py 3
