05e1dcb2810d8c26ae70877344a1545dae66af2c
Brian Koopman <bjk98@cornell.edu>
2016-10-07 13:10:11 -0400

    run.sh: setup to re-run for all three arrays

--- run.sh
+++ run.sh
@@ -3,5 +3,5 @@
 #python wavelength_study.py ./cfg/pa2_wl_study_50_250_ghz_joes_coatings.json
 
 python single_wavelength_run.py 1
-#python single_wavelength_run.py 2
-#python single_wavelength_run.py 3
+python single_wavelength_run.py 2
+python single_wavelength_run.py 3
