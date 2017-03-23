a3084839568eb9d5fc01ff4da986af0bf554efff
Brian Koopman <bjk98@cornell.edu>
2017-03-23 16:54:35 -0400

    optics_tube_decenter: make use of new add_to_json_cfg to add decenter info to output cfg files

--- optics_tube_decenter_systematics.py
+++ optics_tube_decenter_systematics.py
@@ -1,7 +1,7 @@
 # Test polarization angle change as tolerances are introduced. Trying to place
 # uncertainty estiamte on polarization rotation.
 
-import subprocess
+#import subprocess
 import time
 import argparse
 import logging
@@ -27,23 +27,20 @@ tmpDir = "E:\ownCloud\optics\data\\tmp\\"
 #parameter = "x"
 #values = np.arange(-10,11)/10.
 #parameter = "alpha"
-values = np.arange(-20,22,2)/10. #-2 to 2 degrees in 0.2 deg steps
+tilt_values = np.arange(-20, 22, 2)/10. #-2 to 2 degrees in 0.2 deg steps
 
-def test_decenter(parameter, values):
+def test_decenter(parameter, values, units):
     for value in values:
-        # Set ref_wl for file descriptors.
-        if ARRAY in ['1', '2']:
-            ref_wl = 2070000
-        elif ARRAY in ['3']:
-            raise ValueError("Array 3 not yet supported.")
-            #ref_wl = 3000000
-
         if parameter in ['x', 'y', 'z']:
             descriptors = [parameter, "%smm"%(int(value*10))] # Note, this is done to avoid a decimal in the filename!
         else:
-            descriptors = [parameter, "%sdeg"%(str(value).replace('.','p'))]
+            descriptors = [parameter, "%sdeg"%(str(value).replace('.', 'p'))]
         # Build .seq file for automated run.
         arc_autov = autov.AutoV(ARRAY, descriptors)
+
+        decenter_description_dict = {"parameter": parameter, "offset": value, "units": units}
+        arc_autov.add_to_json_cfg("decenter", decenter_description_dict)
+
         arc_autov.create_header()
         arc_autov.load_clean_len()
         arc_autov.remove_glass()
@@ -70,32 +67,8 @@ def test_decenter(parameter, values):
 
         arc_autov.exit()
         arc_autov.run()
-        arc_autov.save_cfg()
+        arc_autov.save_cfg(out_dir="./output/%s_decenter/"%(parameter))
 
-test_decenter("alpha", values)
-test_decenter("beta", values)
+test_decenter("alpha", tilt_values, "deg")
+#test_decenter("beta", values, "deg")
 #test_decenter(parameter, [-2.0])
-
-# Old script before looping.
-## Build .seq file for automated run.
-#qq = autov.AutoV(ARRAY)
-#qq.create_header()
-#qq.load_clean_len()
-#qq.remove_glass()
-#qq.apply_ar_coatings(coating_file = r"E:\ownCloud\optics\mul\ar_coating_sys\two_layer_coating_138_m5_250_m5.mul")
-#if ARRAY in ['1', '2']:
-#    qq.set_wavelengths(wavelengths=[2140000, 2070000, 2000000], reference=1) # ar1, ar2
-#    ref_wl = 2070000
-#elif ARRAY in ['3']:
-#    qq.set_wavelengths(wavelengths=[3000000, 2070000, 1380000], reference=0) # ar3
-#    ref_wl = 3000000
-#qq.set_fields()
-#qq.set_vignetting()
-#qq.activate_pol_ray_trace()
-#qq.set_image_semi_aperture()
-#qq.run_psf([str(int(ref_wl))])
-#qq.run_real_ray_trace(file_descriptors=[str(int(ref_wl))])
-#qq.run_poldsp(input_angle=0, file_descriptors=[str(int(ref_wl))], pupil_number=23)
-#qq.run_poldsp(input_angle=90, file_descriptors=[str(int(ref_wl))], pupil_number=23)
-#qq.exit()
-#qq.run()
