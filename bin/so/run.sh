18088711bafe351524fb52894c56cd8b3b6fd287
Brian Koopman <bjk98@cornell.edu>
2017-04-10 17:15:09 -0400

    Add run.sh file for running multiple tolerancing.py scripts

---
+++ bin/so/run.sh
@@ -0,0 +1,19 @@
+# Brian Koopman
+
+#python tolerancing.py DLX 5
+python tolerancing.py DLY 5
+python tolerancing.py DLZ 5
+python tolerancing.py DLA 5
+python tolerancing.py DLB 5
+
+python tolerancing.py DLX 8
+python tolerancing.py DLY 8
+python tolerancing.py DLZ 8
+python tolerancing.py DLA 8
+python tolerancing.py DLB 8
+
+python tolerancing.py DLX 9
+python tolerancing.py DLY 9
+python tolerancing.py DLZ 9
+python tolerancing.py DLA 9
+python tolerancing.py DLB 9
