02450aef4f1bd8348ef14d1310d451c9e7845736
Brian Koopman <bjk98@cornell.edu>
2018-03-03 17:42:51 -0500

    Add polcal.py to Makefile for automation within Windows

--- bin/polcal/run.sh
+++ bin/polcal/run.sh
@@ -1,3 +1,5 @@
+# depreciated by use of Makefile
+# should move these to Makefile once workflow is established
 python polcal_ar1234.py 1 150
 python polcal_ar1234.py 2 150
 python polcal_ar1234.py 3 90
