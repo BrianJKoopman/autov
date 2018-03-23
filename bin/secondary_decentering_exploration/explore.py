f4d9ff9f6b2a65c3094d2440925aceb981edf592
Brian Koopman <bjk98@cornell.edu>
2018-03-23 11:58:46 -0400

    Generalize and setup for all decenter types

--- bin/secondary_decentering_exploration/explore.py
+++ bin/secondary_decentering_exploration/explore.py
@@ -23,37 +23,49 @@ logging.basicConfig(filename='./explore.log', format='%(asctime)s - %(levelname)
 logging.debug("Logging started.")
 
 ARRAY = args.array
-DATE = time.strftime('%Y%m%d')
-CTIME = int(time.time())
 
-outDir = "E:\ownCloud\optics\data\\"
-tmpDir = "E:\ownCloud\optics\data\\tmp\\"
+def explore_decenter_parameter_space(decenter_parameter, values):
+    for _value in values:
+        print _value
+        # Build .seq file for automated run.
+        TEL = autoact.AutoACT(ARRAY, descriptors=["secondary_decentering", "pa%s"%(ARRAY), decenter_parameter, _value])
+        TEL.create_header()
+        TEL.load_clean_len()
+        TEL.remove_glass()
+        TEL.apply_ar_coatings()
+        TEL.set_wavelengths(wavelengths=[int(autov.freq2lambda(int(args.frequency)))], reference=0)
+    
+        # surface 4 is the secondary
+        TEL.set_decenter_type(4, 'decenter and return')
+        TEL.decenter_surface(4, decenter_parameter, _value)
+    
+        TEL.set_fields(polarization=1)
+        TEL.set_vignetting()
+        TEL.activate_pol_ray_trace()
+        TEL.set_image_semi_aperture()
+        TEL.run_psf()
+        TEL.run_real_ray_trace()
+    
+        # skipping polarization analysis, since it'll just slow us down for now
+        #TEL.run_poldsp(input_angle=0, pupil_number=23)
+        #TEL.run_poldsp(input_angle=90, pupil_number=23)
+    
+        TEL.exit()
+        TEL.run()
+        file_name = TEL.save_cfg(out_dir="./output/")
 
-tilts = np.arange(0, 0.19, 0.01)
-#tilts = [0.18, 0.19]
-decenter_parameter = 'beta'
 
-for tilt in tilts:
-    print tilt
-    # Build .seq file for automated run.
-    TEL = autoact.AutoACT(ARRAY, descriptors=["secondary_decentering", "pa%s"%(ARRAY), decenter_parameter, tilt])
-    TEL.create_header()
-    TEL.load_clean_len()
-    TEL.remove_glass()
-    TEL.apply_ar_coatings()
-    TEL.set_wavelengths(wavelengths=[int(autov.freq2lambda(int(args.frequency)))], reference=0)
+# alpha
+explore_decenter_parameter_space('alpha', np.arange(-0.17, 0.17, 0.01))
 
-    TEL.set_decenter_type(4, 'decenter and return')
-    TEL.decenter_surface(4, 'alpha', tilt)
+# beta
+explore_decenter_parameter_space('beta', np.arange(-0.17, 0.17, 0.01))
 
-    TEL.set_fields(polarization=1)
-    TEL.set_vignetting()
-    TEL.activate_pol_ray_trace()
-    TEL.set_image_semi_aperture()
-    TEL.run_psf()
-    TEL.run_real_ray_trace()
-    #TEL.run_poldsp(input_angle=0, pupil_number=23)
-    #TEL.run_poldsp(input_angle=90, pupil_number=23)
-    TEL.exit()
-    TEL.run()
-    file_name = TEL.save_cfg(out_dir="./output/")
+# x
+explore_decenter_parameter_space('x', np.arange(-0.9, 1, 0.1))
+
+# y
+explore_decenter_parameter_space('y', np.arange(-0.9, 0.7, 0.1))
+
+# z
+explore_decenter_parameter_space('z', np.arange(-1.6, 2.0, 0.1))
